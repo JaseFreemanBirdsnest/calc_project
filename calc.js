5c8b2c2f0d8d6f5bd59db8d2bcabd5d485d2e35c
JaseFreemanBirdsnest <jason.freeman@birdsnest.com.au>
2023-02-28 14:42:43 +1100

    display is mostly functional, currently passes a string to a domath function

--- calc.js
+++ calc.js
@@ -1,14 +1,70 @@
-const btnPress = document.querySelectorAll("div.button")
-const addOnClickEvent = document.getElementById("buttons");
+let currentNum = "";
+let prevNum = "";
+let runningNum = "";
+let operator = "";
 
-function getButtonPressed(){
-    const press = btnPress.addEventListener('click', (e) => {
-        e.id;
-    });
-    console.log(press);
+const numPress = document.querySelectorAll(".numBtn");
+const operatorPress = document.querySelectorAll(".operator");
+const equalsPress = document.querySelector(".equals");
+const clearPress = document.querySelector(".clear");
+const lrgDisplay = document.getElementById("largetype");
+const smlDisplay = document.getElementById("smalltype");
+
+numPress.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    getNumPressed(e.target.textContent);
+  });
+});
+
+operatorPress.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    getOpPressed(e.target.textContent);
+  });
+});
+
+equalsPress.addEventListener("click", (e) => {
+  getEqPressed(e.target.textContent);
+});
+
+clearPress.addEventListener("click", (e) => {
+  getClearPressed(e.target.textContent);
+});
+
+function getNumPressed(press) {
+  currentNum = press;
+  lrgDisplay.textContent += currentNum;
+  runningNum = runningNum + currentNum;
 }
 
-function addOnClick(){
-    let divToAddTo = addOnClickEvent.querySelectorAll("button");
-    divToAddTo.setAttribute("onClick", "getButtonPressed()");
+function getOpPressed(press) {
+  if (!prevNum) {
+    prevNum = runningNum + " " + press;
+    smlDisplay.textContent = prevNum;
+    currentNum = "";
+    runningNum = "";
+    lrgDisplay.textContent = currentNum;
+  } else {
+    prevNum = prevNum + " " + runningNum + " " + press;
+    smlDisplay.textContent = prevNum;
+    currentNum = "";
+    runningNum = "";
+    lrgDisplay.textContent = currentNum;
+  }
+}
+
+function getEqPressed(press) {
+  prevNum = prevNum + " " + runningNum + " " + press;
+  smlDisplay.textContent = prevNum;
+  currentNum = "";
+  runningNum = "";
+  lrgDisplay.textContent = doMath(prevNum);
+  console.log(press);
+}
+
+function getClearPressed(press) {
+  console.log(press);
+}
+
+function doMath(equation) {
+  console.log("equation=" + equation);
 }
